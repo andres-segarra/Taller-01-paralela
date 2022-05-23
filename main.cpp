84d0c24ffee1c188a33f6ef6521937caea6ff834
andres-segarra <52360497+andres-segarra@users.noreply.github.com>
2022-05-22 22:48:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,12 @@
 #include <vector>
 #include <iomanip>
 #include <omp.h>
-#define NOMBRE_ARCHIVO "pruebas.csv"
-
-//Tag de envio
-#define TAG 1
-
-
-// Indica el procesador que será considerado como Líder (Master)
-
-#define LIDER 0
+#define NOMBRE_ARCHIVO "testeo.csv"
 
 //using
 using namespace std;
 
-/**
- * @param argc Cantidad de argumentos
- * @param argv El arreglo asociativo con los argumentos del programa
- * @return El programa retornará Cero en caso de éxito y un número distinto en
- * caso de error.
- */
-
-int main(/*int argc, char *argv[]*/) {
-
+int main() {
    // Variables auxiliares
     int aux = 0;
     int aux2 = 0;
@@ -44,13 +28,11 @@ int main(/*int argc, char *argv[]*/) {
     float puntajetotal =0;
     float nota =0;
     float resul = 0.0;
-
     std::ifstream lector (NOMBRE_ARCHIVO);
     string linea;
     char delimitador =';';
     getline (lector, linea);
     string recibidor [13];
-
     std::ofstream Resultado; //aca se creó el archivo csv
      if (Resultado.fail()){
             printf("ERROR AL LEER EL ARCHIVO"); // no se pudo abrir/crera archivo
@@ -58,12 +40,10 @@ int main(/*int argc, char *argv[]*/) {
         }
     Resultado.open ("Resultados.csv");
     Resultado << "Token Estudiante"<<";"<<"Correctas"<<";"<<"Incorrectas"<<";"<<"Omitidas"<<";"<<"Puntaje"<<";"<<"Nota"<< "\n";
-#pragma omp parallel
-{
-    //int id=omp_get_thread_num();
-    //int nt= omp_get_num_threads();
 
-    while (getline(lector, linea)){
+    #pragma omp parallel
+{
+     while (getline(lector, linea)){
         stringstream stream(linea);
         string nombre, p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12;
         getline(stream, nombre, delimitador);
@@ -92,9 +72,8 @@ int main(/*int argc, char *argv[]*/) {
         recibidor[10] = p10;
         recibidor[11] = p11;
         recibidor[12] = p12;
+        #pragma omp for
 
-
-#pragma omp for
         for(int i=1; i < 13; i++){
             if(recibidor[i] == correctas[i]){
                 notaaux += 0.5;
@@ -108,11 +87,10 @@ int main(/*int argc, char *argv[]*/) {
                 }
             }
         }
+        #pragma omp critical
         if(notaaux<1){  //si ya teniendo 9 malas es nota 1, se ja como esta nota como minima
             notaaux = 1.0;
         }
-
-#pragma omp critical
 
         Resultado << recibidor[0] <<";"<< rcorrectas <<";"<< rmalas <<";"<<romitidas <<";"<< (rcorrectas*0.5-rmalas*0.12) <<";"<< fixed << setprecision(1) << notaaux << "\n";
 
@@ -125,12 +103,12 @@ int main(/*int argc, char *argv[]*/) {
     lector.close();
     Resultado.close ();
 }
-    cout << "----------------" << endl;
+   cout << "----------------" << endl;
     cout << "=== Integrantes ===" << endl;
     cout << "Luis Rivas Sanchez" << endl;
     cout << "Rodrigo Mora Palta" << endl;
     cout << "Andres Segarra Pavez" << endl;
     cout << "----------------" << endl;
-    return 0;
 
+    return 0;
 }
