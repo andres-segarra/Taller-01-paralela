cbdb044e236e40d99f3062c3c8a0752e8564f11a
andres-segarra <52360497+andres-segarra@users.noreply.github.com>
2022-05-29 22:09:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,40 +14,40 @@ using namespace std;
 int main()
 {
     // variable con las respuestas correctas
-    string correctas[13] = {"-", "A", "E", "C", "B", "B", "D", "A", "B", "E", "C", "B", "D"};
-    ifstream lector;
-    string linea;
-    // aca se abre el archivo que se va a leer
+    string correctas[13] = {"-", "A", "E", "C", "B", "B", "D", "A", "B", "E", "C", "B", "D"}; // Arreglo con las respuestas correctas
+    ifstream lector;    // La variable lector crea un espacio donde se cargara la información del documento a leer
+    string linea;       // Variable multiuso para la lectura de filas
+    // Aca se abre el archivo que se va a leer
     lector.open("pruebas.csv", ios::in);
     if (lector.fail())
-    { // por si no se pudo abrir el archivo
+    { // Por si no se pudo abrir el archivo
         cout << "ERROR AL LEER EL ARCHIVO" << endl;
         exit(1);
     }
-    char delimitador = ';';
-    getline(lector, linea);
-    ofstream Resultado; // aca se crea el archivo csv
+    char delimitador = ';'; // Caracter que delimita las distintas columnas de las filas
+    getline(lector, linea); // Con esta acción se pasará directamente a la fila de estudiantes
+    ofstream Resultado;     // Aca se crea el archivo csv
     if (Resultado.fail())
-    { // no se pudo crear el archivo
+    { // No se pudo crear el archivo
         cout << "ERROR AL CREAR EL ARCHIVO DE SALIDA" << endl;
         exit(1);
     }
-    // se crea/reescribe el archivo resultados.csv, y procede en llenar lo que seria la primera fila del archivo
+    // Se reescribe el archivo Resultados.csv, y procede en llenar lo que seria la primera fila del archivo = El encabezado
     Resultado.open("Resultados.csv", ios::out);
     Resultado << '"' << "Token Estudiante" << '"' << ';' << '"' << "Correctas" << '"' << ';' << '"' << "Incorrectas" << '"' << ';' << '"' << "Omitidas" << '"' << ';' << '"' << "Puntaje" << '"' << ';' << '"' << "Nota" << '"' << "\n";
-
+    getline(lector, linea);
     for (int m = 0; m >= 0; m++)
     {
-        // codigo
-        string recibidor[13];
-        int rcorrectas = 0;
-        int rmalas = 0;
-        int romitidas = 0;
-        float notaaux = 1;
+        // Codigo base
+        string recibidor[13]; // Arreglo de 13 espacios para recibir las filas del archivo
+        int rcorrectas = 0;   // Contador de preguntas correctas
+        int rmalas = 0;       // Contador de preguntas incorrectas
+        int romitidas = 0;    // Contador de preguntas omitidas
+        float notaaux = 1;    // Nota base
         stringstream stream(linea);
-        string nombre, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12;
+        string nombre, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12; // Variables vacias donde guardaremos información de la fila
         getline(lector, linea);
-        getline(stream, nombre, delimitador);
+        getline(stream, nombre, delimitador);//Ahora se guardaran los datos de la fila en cada variable deteniendose en cuando encuentren el delimitador previamente detallado
         getline(stream, p1, delimitador);
         getline(stream, p2, delimitador);
         getline(stream, p3, delimitador);
@@ -60,7 +60,7 @@ int main()
         getline(stream, p10, delimitador);
         getline(stream, p11, delimitador);
         getline(stream, p12, delimitador);
-        recibidor[0] = nombre;
+        recibidor[0] = nombre;//Cada dato guardado en las variables ahora formará parte del arreglo recibidor para analizarlo
         recibidor[1] = p1;
         recibidor[2] = p2;
         recibidor[3] = p3;
@@ -73,25 +73,25 @@ int main()
         recibidor[10] = p10;
         recibidor[11] = p11;
         recibidor[12] = p12;
-#pragma omp parallel num_threads(3)//definimos la cantidad de hilos a trabajar, en este caso 3
+#pragma omp parallel num_threads(3) // Definimos la cantidad de hilos a trabajar, en este caso 3
 #pragma omp for
         for (int i = 1; i < 13; i++)
         {
-            // se compara la entrada con que si la pregunta es correcta
+            // Se compara la entrada con que si la pregunta es correcta
 #pragma omp critical
-            if (recibidor[i] == ('"' + correctas[i] + '"'))
+            if (recibidor[i] == ('"' + correctas[i] + '"')) // Se compara el dato de entrada con las respuestas correctas
             {
                 notaaux += 0.5;
                 rcorrectas++;
             }
             else
             {
-                if (recibidor[i] == '"' + correctas[0] + '"')
+                if (recibidor[i] == '"' + correctas[0] + '"') // Se compara con el valor de la posicion correctas[0] que es "-"
                 {
                     romitidas++;
                 }
                 else
-                { // sino es una pregunta mal respondida
+                { // Sino es una pregunta mal respondida
                     notaaux -= 0.12;
                     rmalas++;
                 }
@@ -99,25 +99,25 @@ int main()
         }
 
         if (notaaux < 1)
-        { // si es nota menor a 1, se deja como esta nota como minima
+        { // Si es nota menor a 1, se deja como esta nota como minima
             notaaux = 1.0;
         }
 
-        if (lector.eof())
+        if (lector.eof())  // Se corrobora si el archivo esta vacio
         {
             m = -10;
         }
         else
-        {
+        {   //Aca se meten los valores en el archivo, llamado Resultado
             Resultado << recibidor[0] << ';' << '"' << rcorrectas << '"' << ';' << '"' << rmalas << '"' << ';' << '"' << romitidas << '"' << ';' << '"' << (rcorrectas * 0.5 - rmalas * 0.12) << '"' << ';' << '"' << fixed << setprecision(1) << notaaux << '"' << "\n"; // se llena el archivo con lo datos
-            rcorrectas = 0;                                                                                                                                                                                                                                               // reestablecer valores de variables
+            rcorrectas = 0;       // Se reinician los valores de las variables                                                                                                                                                                                                                                        // reestablecer valores de variables
             rmalas = 0;
             romitidas = 0;
             notaaux = 1.0;
         }
-    } // cerrado del for
+    } 
 
-    lector.close(); // se cierran los archivos
+    lector.close(); // Se cierran los archivos
     Resultado.close();
 
     cout << "----------------" << endl;
@@ -129,4 +129,3 @@ int main()
 
     return 0;
 }
-// cierre main
